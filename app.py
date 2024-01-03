014e4f22960a3dd77b492ea97a92bafa39655c63
Maciej <maciek8131@protonmail.com>
2024-01-03 15:33:43 +0000

    Odcinek 12

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask, config, render_template, request, redirect, url_for, se
 from pprint import pprint
 from jinja2 import Environment, FileSystemLoader
 from tabulate import tabulate
+import yaml
+
+
+
 
 app = Flask(__name__)
 
 
 routes={}
-routes["Szablony"]=[["/templates_router","Szablony dla routera","Opis","warning"]]
-
+routes["Odcinek 12"]=[["/inventory_nornir","Nornir Inventory","","success"]]
 
 @app.route("/")
 def homepage():
     return render_template("base_site.html",routes=routes)
 
-@app.route("/templates_router")
-def templates_router():
-    return render_template("templates_router.html")
+
+@app.route("/inventory_nornir")
+def inventory_nornir():
+    inventory=yaml.load(open("static/nornir/hosts.yaml","r"),Loader=yaml.SafeLoader)
+    return render_template("inventory_nornir.html",inventory=inventory)
 
 
 
