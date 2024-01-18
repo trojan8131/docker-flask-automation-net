a910ba5c90c358727266adf165968f0c12cee067
Maciej <maciek8131@protonmail.com>
2024-01-18 17:55:38 +0000

    Odcinek 17

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # docker-flask-automation-net
 docker-flask-automation-net
+
+
+
+# Funkcje strony:
+1. Zarządzenia inventory Nornirowym
+2. Generowanie konfiguracji urządzeń sieciowych
+3. Wysyłanie konfiguracji na urządzenia po SSH oraz tych podłączonych kablem konsolowym
+4. Baza posiadanych urządzeń w danych lokalizacjach
+
+
+
+
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from nornir_inspect import nornir_inspect
 
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
+                     RadioField,SubmitField)
 from wtforms.validators import InputRequired, Length
 from jinja2 import Template
 
@@ -18,20 +18,42 @@ from nornir_netmiko.tasks import netmiko_send_config
 from nornir.core.inventory import Host,Group
 from nornir_rich.functions import print_result
 
-
-
+from pymongo import *
 
 ############ Flask Config ##########
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 
+
+
+########### MongoDB #############
+
+client= MongoClient('172.19.0.7', 27017,username="autonet",password="password")
+autonet_db=client.autonet
+mongo_database= autonet_db.routery_stacje_benzynowe
+
+#Inicjalizacja bazy
+# index_model = ["number","location","subnet","public_ip"]
+# for index in index_model:
+#     mongo_database.create_index(index, unique=True)
+
+# Dodawanie obietków
+# petrol_station={
+#     "number": 1000,
+#     "location": "Warszawa Parkowa",
+#     "subnet": "172.16.0.0/24",
+#     "public_ip": "3.4.5.6"
+# }
+# mongo_database.insert_one(petrol_station)
+
 ########### Routes ###########
 
 
 
 routes={"Odcinek 13/14":[["/inventory_nornir","Nornir Inventory","Urządzenia z hosts.yaml","success"]],
-        "Odcinek 15/16":[["/router_config_form","Formularz konfiguracji","Formularz konfiguracji routera","success"]]
+        "Odcinek 15/16":[["/router_config_form","Formularz konfiguracji","Formularz konfiguracji routera","success"]],
+        "Odcinek 17":[["/petrol_station_database","Baza stacji beznzynowych","Baza w MongoDB","success"]]
 }
 
 
@@ -46,6 +68,7 @@ class DeviceForm(FlaskForm):
                        validators=[InputRequired()])
     vlan_dhcp = BooleanField('Vlan DHCP')
     send_config = BooleanField("Czy wysłać konfigurację do podłączonego urządzenia")
+
 
 
 
@@ -116,11 +139,10 @@ def router_config_form():
     return render_template('router_config_form.html', form=form)
 
 
-
-
-
-
-
+@app.route('/petrol_station_database', methods=['POST',"GET"])
+def petrol_station_database():
+    stations=list(mongo_database.find().sort("number"))
+    return render_template('petrol_station_database.html',stations=stations)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
